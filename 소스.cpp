c4fdcb4b30fe890aa65a39d3694e20e539c8d9d9
오서현 <48465715+dlsanf2000@users.noreply.github.com>
2019-04-11 14:26:00 +0900

    5장 예제
    명품 C++책 5장 예제입니다.

--- 소스.cpp
+++ 소스.cpp
@@ -1,319 +1,465 @@
 #include <iostream>
 using namespace std;
 
-
-//예제 6-1
-/*
-int big(int a, int b) {
-	if (a > b) return a;
-	else return b;
-}
-
-int big(int a[], int size) {
-	int res = a[0];
-	for (int i = 1; i < size; i++)
-		if (res < a[i]) res = a[i];
-	return res;
-}
-
-int main() {
-	int array[5] = { 1,9, -2, 8, 6 };
-	cout << big(2, 3) << endl;
-	cout << big(array, 5) << endl;
-}
-*/
-
-
-//예제 6-2
-/*
-int sum(int a, int b) {
-	int s = 0;
-	for (int i = a; i <= b; i++)
-		s += i;
-	return s;
-}
-
-int sum(int a) {
-	int s = 0;
-	for (int i = 0; i <= a; i++)
-		s += i;
-	return s;
-}
-
-int main() {
-	cout << sum(3, 5) << endl;
-	cout << sum(3) << endl;
-	cout << sum(100) << endl;
-}
-*/
-
-
-//예제 6-3
-/*
-#include <string>
-
-//원형 선언
-void star(int a = 5);
-void msg(int id, string text = "");
-
-//함수 구현
-void star(int a) {
-	for (int i = 0; i < a; i++) cout << '*';
-	cout << endl;
-}
-
-void msg(int id, string text) {
-	cout << id << ' ' << text << endl;
-}
-
-int main() {
-	star();
-	star(10);
-
-	msg(10);
-	msg(10, "Hello");
-}
-*/
-
-
-//예제 6-4
-/*
-//원형 선언
-void f(char c = ' ', int line = 1);
-
-//함수 구현
-void f(char c, int line) {
-	for (int i = 0; i < line; i++) {
-		for (int j = 0; j < 10; j++)
-			cout << c;
-		cout << endl;
-	}
-}
-
-int main() {
-	f();
-	f('%');
-	f('@', 5);
-}
-*/
-
-//예제 6-5
-
-/*
-void fillLine(int n = 25, char c = '*') {
-	for (int i = 0; i < n; i++)
-		cout << c;
-	cout << endl;
-}
-int main() {
-	fillLine();
-	fillLine(10, '%');
-}
-*/
-
-//예제 6-6
-
-/*
-class MyVector {
-	int *p;
-	int size;
-public:
-	MyVector(int n = 100) {
-		p = new int[n];
-		size = n;
-	}
-	~MyVector() { delete[] p; }
-};
-int main() {
-	MyVector *v1, *v2;
-	v1 = new MyVector();
-	v2 = new MyVector(1024);
-
-	delete v1;
-	delete v2;
-}
-*/
-
-//예제 6-7
-/*
-
-float square(float a) {
-	return a * a;
-}
-
-double square(double a) {
-	return a * a;
-}
-int main() {
-	cout << square(3.0);
-	cout << square(3);
-}
-*/
-
-//예제 6-8
-/*
-int add(int a, int b) {
-	return a + b;
-}
-
-int add(int a, int &b) {
-	b = b + a;
-	return b;
-}
-
-int main() {
-	int s = 10, t = 20;
-	cout << add(s, t);	//오버로드된 함수 add의 인스턴스 중 두 개 이상이 인수목록과 일치함 , 함수 중복의 모호성
-}
-*/
-
-//예제 6-9
-/*
-#include <string>
-
-void msg(int id) {
-	cout << id << endl;
-}
-void msg(int id, string s = "") {
-	cout << id << ":" << s << endl;
-}
-
-int main() {
-	msg(5, "Good Morning");
-	msg(6);	//함수 호출 모호해서 컴파일이 오류남
-}
-*/
-
-//예제 6-10
-/*
-class Math {
-public:
-	static int abs(int a) {
-		return a > 0 ? a : -a;
-	}
-	static int max(int a, int b) {
-		return (a > b) ? a : b;
-	}
-	static int min(int a, int b) {
-		return (a > b) ? b : a;
-	}
-};
-
-int main() {
-	cout << Math::abs(-5) << endl;
-	cout << Math::max(10, 8) << endl;
-	cout << Math::min(-3, -8) << endl;
-}
-
-*/
-
-//예제 6-11
+//예제 5-1
 /*
 class Circle {
 private:
-	static int numOfCircles;
 	int radius;
 public:
-	Circle(int r = 1);
-	~Circle() {
-		numOfCircles--;
+	Circle();
+	Circle(int r);
+	~Circle();
+	double getArea() {
+		return 3.14*radius*radius;
+	}
+	int getRadius() {
+		return radius;
+	}
+	void setRadius(int radius) {
+		this->radius = radius;
+	}
+};
+
+Circle::Circle() {
+	radius = 1;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::Circle(int radius) {
+	this->radius = radius;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+Circle::~Circle() {
+	cout << "소멸자 실행 radius =" << radius << endl;
+}
+
+void increase(Circle c) {
+	int r = c.getRadius();
+	c.setRadius(r + 1);
+}
+
+int main() {
+	Circle waffle(30);
+	increase(waffle);
+	cout << waffle.getRadius() << endl;
+}
+
+*/
+
+
+//예제 5-2
+/*
+class Circle {
+	int radius;
+public:
+	Circle() { radius = 1; }
+	Circle(int radius) { this->radius = radius; }
+	void setRadius(int radius) { this->radius = radius; }
+	double getArea() { return 3.14*radius*radius; }
+};
+
+Circle getCircle() {
+	Circle tmp(30);
+	return tmp;
+}
+int main() {
+	Circle c;
+	cout << c.getArea() << endl;
+
+	c = getCircle();
+	cout << c.getArea() << endl;
+}
+*/
+
+//예제 5-3
+/*
+int main() {
+	cout << "i" << '\t' << "n" << '\t' << "refn" << endl;
+	int i = 1;
+	int n = 2;
+	int &refn = n;
+
+	n = 4;
+	refn++;
+	cout << i << '\t' << n << '\t' << refn << endl;
+
+	refn = i;
+	refn++;
+	cout << i << '\t' << n << '\t' << refn << endl;
+
+	refn = i;
+	refn++;
+	cout << i << '\t' << n << '\t' << refn << endl;
+
+	int *p = &refn;
+	*p = 20;
+	cout << i << '\t' << n << '\t' << refn << endl;
+}
+*/
+
+//예제 5-4
+/*
+class Circle {
+	int radius;
+public:
+	Circle() { radius = 1; }
+	Circle(int radius) {
+		this->radius = radius;
+	}
+	void setRadius(int radius) {
+		this->radius = radius;
 	}
 	double getArea() {
 		return 3.14*radius*radius;
 	}
-	static int getNumOfCircles() {
-		return numOfCircles;
-	}
 };
 
-Circle::Circle(int r) {
-	radius = r;
-	numOfCircles++;
-}
-
-int Circle::numOfCircles = 0;
-
 int main() {
-	Circle *p = new Circle[10];
-	cout << "생존하고 있는 원의 개수 = " << Circle::getNumOfCircles() << endl;
+	Circle circle;
+	Circle &refc = circle;
+	refc.setRadius(10);
+	cout << refc.getArea() << " " << circle.getArea();
+}
+*/
 
-	delete[] p;
-	cout << "생존하고 있는 원의 개수 = " << Circle::getNumOfCircles() << endl;
+//예제 5-5
 
-	Circle a;
-	cout << "생존하고 있는 원의 개수 =" << Circle::getNumOfCircles() << endl;
+/*
+bool average(int a[], int size, int& avg) {
+	if (size <= 0)
+		return false;
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+		sum += a[i];
+	avg = sum / size;
+	return true;
+}
+int main() {
+	int x[] = { 0,1,2,3,4,5 };
+	int avg;
+	if (average(x, 6, avg)) cout << "평균은" << avg << endl;
+	else cout << "매개변수 오류" << endl;
 
-	Circle b;
-	cout << "생존하고 있는 원의 개수 = " << Circle::getNumOfCircles() << endl;
+	if (average(x, -2, avg))cout << "평균은" << avg << endl;
+	else cout << "매개 변수오류" << endl;
 
 }
 */
 
-//그림 6-11, static 멤버의 선언과 활용, 객체 이름으로 접근
-
+//예제 5-6
 /*
-class Person {
+class Circle {
+private:
+	int radius;
 public:
-	double money;
-	void addMoney(int money) {
-		this->money += money;
+	Circle();
+	Circle(int r);
+	~Circle();
+	double getArea() {
+		return 3.14*radius*radius;
 	}
-	static int shareMoney;
-	static void addShared(int n) {
-		shareMoney += n;
+	int getRadius() {
+		return radius;
+	}
+	void setRadius(int radius) {
+		this->radius = radius;
+	}
+};
+Circle::Circle() {
+	radius = 1;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+Circle::Circle(int radius) {
+	this->radius = radius;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+Circle::~Circle() {
+	cout << "소멸자 실행 radius = " << radius << endl;
+}
+
+void increase(Circle &c) {
+	int r = c.getRadius();
+	c.setRadius(r + 1);
+}
+int main() {
+	Circle waffle(30);
+	increase(waffle);
+	cout << waffle.getRadius() << endl;
+}
+*/
+
+//예제 5-7
+/*
+class Circle {
+	int radius;
+public:
+	Circle() {
+		radius = 1;
+	}
+	Circle(int radius) {
+		this->radius = radius;
+	}
+	void setRadius(int radius) {
+		this->radius = radius;
+	}
+	double getArea() {
+		return 3.14*radius*radius;
 	}
 };
 
-//static 변수 생성, 전역공간에 생성
-int Person::shareMoney = 10;
+void readRadius(Circle &c) {
+	int r;
+	cout << "정수 값으로 반지름을 입력하세요>>";
+	cin >> r;
+	c.setRadius(r);
+}
 
-//main 함수
 int main() {
-	Person han;
-	han.money = 100;
-	han.shareMoney = 200;
+	Circle donut;
+	readRadius(donut);
+	cout << "donut의 면적 = " << donut.getArea() << endl;
+}
 
-	Person lee;
-	lee.money = 150;
-	lee.addMoney(200);
-	lee.addShared(200);
+*/
 
-	cout << han.money << ' ' << lee.money << endl;
-	cout << han.shareMoney << ' ' << lee.shareMoney << endl;
+//예제 5-8
+
+/*
+//배열 s의 index 원소 공간에 대한 참조를 리턴하는 함수
+char& find(char s[], int index) {
+	return s[index];
+}
+
+int main() {
+	char name[] = "Mike";
+	cout << name << endl;
+
+	find(name,0) = 'S';
+	cout << name << endl;
+
+	char& ref = find(name, 2);
+	ref = 't';
+	cout << name << endl;
+
+}
+*/
+
+//예제 5-9
+
+/*
+class Circle {
+private:
+	int radius;
+public:
+	Circle(Circle& c);
+	Circle() {
+		radius = 1;
+	}
+	Circle(int radius) {
+		this->radius = radius;
+	}
+	double getArea() {
+		return 3.14*radius*radius;
+	}
+};
+
+Circle::Circle(Circle& c) {
+	this->radius = c.radius;
+	cout << "복사 생성자 실행 radius = " << radius << endl;
+}
+
+int main() {
+	Circle src(30);
+	Circle dest(src);
+
+	cout << "원본의 면적 = " << src.getArea() << endl;
+	cout << "사본의 면적 = " << dest.getArea() << endl;
 }
 */
 
 
+//예제 5-10
 
-//클래스 명으로 static 멤버를 접근하는 사례
 /*
+#include <cstring>
+#pragma warning ( disable : 4996 )
+
 class Person {
+	char* name;
+	int id;
 public:
-	double money;
-	void addMoney(int money) {
-		this->money += money;
-	}
-	static int sharedMoney;
-	static void addShared(int n) {
-		sharedMoney += n;
+	Person(int id, const char* name);
+	~Person();
+	void changeName(const char* name);
+	void show() {
+		cout << id << "," << name << endl;
 	}
 };
 
-//static 변수 생성. 전역 공간에 생성
-int Person::sharedMoney = 10;	//10으로 초기화
+Person::Person(int id, const char* name) {
+	this->id = id;
+	int len = strlen(name);
+	this->name = new char[len + 1];
+	strcpy(this->name, name);
+}
 
-//main() 함수
+Person::~Person() {
+	if (name)
+		delete[] name;
+}
+
+void Person::changeName(const char* name) {
+	if (strlen(name) > strlen(this -> name))
+		return;
+	strcpy(this -> name, name);
+}
+
 int main() {
-	Person::addShared(50);
-	cout << Person::sharedMoney << endl;
+	Person father(1, "Kitae");
+	Person daughter(father);
 
-	Person han;
-	han.money = 100;
-	han.sharedMoney = 200;
-	Person::sharedMoney = 300;
-	Person::addShared(100);
+	cout << "daughter 객체 생성 직후 ----" << endl;
 
-	cout << han.money << ' ' << Person::sharedMoney << endl;
+
+	father.show();
+	daughter.show();
+
+	daughter.changeName("Grace");
+	cout << "daughter 이름을 Grace로 변경한 후 ----" << endl;
+	father.show();
+	daughter.show();
+
+	return 0;
+}
+*/
+
+//예제 5-11
+
+/*
+#include <cstring>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning ( disable : 4996 )
+
+class Person {
+	char* name;
+	int id;
+
+public:
+	Person(int id, const char* name);
+	Person(Person& person);
+	~Person();
+	void changeName(const char *name);
+	void show() {
+		cout << id << ',' << name << endl;
+	}
+};
+
+Person::Person(int id, const char* name) {
+		this->id = id;
+		int len = strlen(name);
+		this->name = new char[len + 1];
+		strcpy(this->name, name);
+}
+
+Person::Person(Person& person) {
+		this->id = person.id;
+		int len = strlen(person.name);
+		this ->name = new char[len + 1];
+		strcpy(this->name, person.name);
+		cout << "복사 생성자 실행. 원본 객체의 이름" << this->name << endl;
+}
+
+Person::~Person() {
+		if (name)
+			delete[] name;
+}
+
+void Person::changeName(const char* name) {
+		if (strlen(name) > strlen(this->name))
+			return;
+		strcpy(this->name, name);
+}
+	   
+int main() {
+	Person father(1, "Kitae");
+	Person daughter(father);
+
+	cout << "daughter 객체 생성 직후 ----" << endl;
+	father.show();
+	daughter.show();
+
+	daughter.changeName("Grace");
+	cout << "daughter 이름을 Grace로 변경한 후 ----" << endl;
+
+	father.show();
+	daughter.show();
+
+	return 0;
+}
+*/
+
+
+//예제 5-12
+
+/*
+#include <cstring>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning ( disable : 4996 )
+
+class Person {
+	char* name;
+	int id;
+
+public:
+	Person(int id, const char* name);
+	Person(Person& person);
+	~Person();
+	void changeName(const char *name);
+	void show() {
+		cout << id << ',' << name << endl;
+	}
+};
+
+Person::Person(int id, const char* name) {
+	this->id = id;
+	int len = strlen(name);
+	this->name = new char[len + 1];
+	strcpy(this->name, name);
+}
+
+Person::Person(Person& person) {
+	this->id = person.id;
+	int len = strlen(person.name);
+	this->name = new char[len + 1];
+	strcpy(this->name, person.name);
+	cout << "복사 생성자 실행. 원본 객체의 이름" << this->name << endl;
+}
+
+Person::~Person() {
+	if (name)
+		delete[] name;
+}
+
+void Person::changeName(const char* name) {
+	if (strlen(name) > strlen(this->name))
+		return;
+	strcpy(this->name, name);
+}
+
+void f(Person person) {
+	person.changeName("dummy");
+}
+Person g() {
+	Person mother(2, "Jane");
+	return mother;
+}
+
+int main() {
+	Person father(1, "Kitae");
+	Person son = father;
+	f(father);
+	g();
 }
 */
 
